de639ba23ccdd449015d42631fc8496cd270d64e
Lolesgoku <143826851+Lolesgoku@users.noreply.github.com>
2024-10-18 12:23:51 -0300

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
 // Cambia estos valores según tu base de datos
-$servername = "localhost"; // o el nombre de tu servidor
-$username = "tu_usuario"; // tu nombre de usuario
-$password = "tu_contraseña"; // tu contraseña
-$dbname = "nombre_de_la_base_de_datos"; // nombre de tu base de datos
+$servername = "sftp.fhgdps.com"; // o el nombre de tu servidor
+$username = "gdps_alexphgdps"; // tu nombre de usuario
+$password = "x3ndg6xi1k9kwe3gackj5e"; // tu contraseña
+$dbname = "gdps_alexphgdps"; // nombre de tu base de datos
 
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
